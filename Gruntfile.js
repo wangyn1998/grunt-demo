bcaeb445a1ff71fcb6ecf013d6ffccd014207996
wangyn <928069834@qq,com>
2020-02-25 17:43:12 +0800

    wyn

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,11 @@ module.exports = function(grunt){
     grunt.initConfig({
         htmlhint:{
             option:{
-                htmlhintrc:'./.htmlhintrc'
+                csslintrc:'./.csslintrc'
             },
-            src:['*.html']
+            src:['*.css']
         }
     });
-    grunt.loadNpmTasks('grunt-htmlhint');
-    grunt.registerTask('default',['htmlhint']);
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.registerTask('default',['csslint']);
 }
