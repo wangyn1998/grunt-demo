965e189b6fb8fa0f2c7456350e1de8e6842f4fe5
wangyn <928069834@qq,com>
2020-02-25 17:20:55 +0800

    wyn

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,12 @@
 module.exports = function(grunt){
     grunt.initConfig({
-        less:{
-            compile:{
-                files:{'dist/compiled.css':'css/*.less'}
-            }
+        htmlhint:{
+            option:{
+                htmlhintrc:'./.htmlhintrc'
+            },
+            src:['*.html']
         }
     });
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.registerTask('default',['less:compile']);
+    grunt.loadNpmTasks('grunt-htmlhint');
+    grunt.registerTask('default',['htmlhint']);
 }
